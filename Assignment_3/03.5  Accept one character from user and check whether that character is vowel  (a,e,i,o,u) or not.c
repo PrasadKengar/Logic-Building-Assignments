c2289e65544800c0cbac71dc50de1047ece44c88
PrasadKengar <kengaraprasad@gmail.com>
2025-05-10 16:33:44 +0530

    Add files via upload

---
+++ Assignment_3/03.5  Accept one character from user and check whether that character is vowel  (a,e,i,o,u) or not.c
@@ -0,0 +1,37 @@
+#include"Header.h"
+
+BOOL ChkVowel ( char ch)
+{
+    if(ch>='a' && ch<='z')
+    {
+        ch=ch-32;
+    }
+    if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter character\n");
+    scanf("%c",&cValue);
+
+    bRet = ChkVowel(cValue);
+
+    if (bRet == TRUE)
+    {
+        printf("It is Vowel");
+    }
+    else
+    {
+        printf("It is not Vowel");
+    }
+    return 0;
+}
